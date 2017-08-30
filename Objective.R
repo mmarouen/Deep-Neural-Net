e1a65c30102f4c53317e8d8f25eb56896b917c2c
azzouz marouen <azzouz.marouen@gmail.com>
2017-08-30 22:50:37 +0200

    Update Objective.R

--- Objective.R
+++ Objective.R
@@ -1,17 +1,18 @@
 #objective function
-objective<-function(X,Y,W,tt="Regression",ll="RSS",outF="Identity",wD,weightsVector){
+objective<-function(X,Y,W,ll="RSS",wD,weightsVector){
   K=ncol(X)
   N=nrow(X)
   #if(tt=="Regression" & ll=="RSS" & outF=="Identity"){
-  if(tt=="Regression" & ll=="RSS"){
+  if(ll=="RSS"){
     #objective=(1/2)*sum(rowMeans((Y-X)^2))
     objective=(1/2)*sum(rowSums(((Y-X)^2)%*%weightsVector))
   }
-  if((tt=="Classification" & outF=="Softmax" & ll=="CrossEntropy")){
+  if(ll=="CrossEntropy"){
     if(K==1){objective=-sum(Y*log(X)+(1-Y)*log(1-X))}
     if(K>1){objective=-sum((Y*log(X))%*%weightsVector)}
   }
   objective=(1/N)*objective
+  #objective=(1/(N*K))*objective
   if(wD[[1]]){
     lambda=wD[[2]]
     s=0
