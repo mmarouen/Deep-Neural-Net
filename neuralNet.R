68b4192da4c179e537e68623def6affd69a8698a
azzouz marouen <azzouz.marouen@gmail.com>
2017-09-05 01:15:12 +0200

    Update neuralNet.R

--- neuralNet.R
+++ neuralNet.R
@@ -35,28 +35,27 @@ neuralNet<-function(Input,response,InputTest=NULL,respTest=NULL, #input data
                     #for now, one activation applies to all layers of the network
                     #options: "tanh", "sigmoid", "linear", "ReLU"
                     rate=0.01,#learning rate
-                    #optAlg:"Adam", "GD", "RMS", "Momentum" (to be included)
-                    #beta1:momentum parameter, default=0.9 (to be included)
-                    #beta2:RMS parameter, default=0.99 (to be included)
-                    #Adam: adam optimization yes/no (to be included)
-                    #mini_batch=NULL: process in minibatches (to be included)
-                                      #mini batch size are in 2^p so user input only exponent term
-                                      #if NULL then whole dateset is taken
-                    #BatchNorm: binary for batch normalization (to be included)
+                    optAlg="GD", #can be 'Adam', 'RMSProp', 'Momentum'
+                    beta1=0.9,#momentum parameter
+                    beta2=0.99,#RMS prop parameter
+                    mini_batch=NULL,#process in minibatches mini batch size are in 2^p so user input only exponent term
+                    #if NULL then whole dateset is taken
+                    #BatchNorm: binary for batch normalization
                     weightDecay=FALSE,lambda=NULL, #weight decay yes/No and decay amount 'lambda'
                     #dropout=FALSE,dropout yes/no, probs=0.7
                     probs=1, #probability
                     gradientCheck=FALSE,traceObj=FALSE,traceWeights=FALSE #debuggers
-                    ){
+){
   t1=Sys.time()
   if(is.null(epochs)){epochs=15000}
   
   rsp=transformResponse(response,type)
-  active=init(DW,Input,rsp$respMat,activationFunc,weightsVector)
-  optimize=OptimizeCost(weight=active$W,resp=rsp,X=active$In,respT=respTest,XT=InputTest,tt=type,
-                 ll=loss,outF=outputFunc,active=activationFunc,rr=rate,
-                 wD=list(weightDecay,lambda),gradientCheck=gradientCheck,traceobj=traceObj,
-                 trW=traceWeights,Epochs=epochs,Tolerance=tol,weightsVec=active$weightsVec)
+  active=init(DW,Input,rsp$respMat,activationFunc,weightsVector,optAlg)
+  optimize=OptimizeCost(weight=active$W,resp=rsp,X=active$In,respT=respTest,XT=InputTest, #data
+                        tt=type,ll=loss,outF=outputFunc,active=activationFunc,rr=rate,minib=mini_batch, #parameters
+                        wD=list(weightDecay,lambda),gradientCheck=gradientCheck,traceobj=traceObj, #debugging
+                        trW=traceWeights,Epochs=epochs,Tolerance=tol,weightsVec=active$weightsVec, #debugging
+                        optimization=optAlg,beta1=beta1,beta2=beta2,momentum=active$momentum,rmsprop=active$rmsprop) 
   t2=Sys.time()
   L=list(dw=DW,tt=type,ll=loss,outF=outputFunc,active=activationFunc,rr=rate,wD=weightDecay,
          lambda=lambda,epochs=epochs)
@@ -66,4 +65,3 @@ neuralNet<-function(Input,response,InputTest=NULL,respTest=NULL, #input data
               scoreTrain=optimize$scoreTrain,scoreTest=optimize$scoreTest,
               trWeights=optimize$wTune,duration=t2-t1,argV=L))
 }
-
