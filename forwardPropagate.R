5d0886677ce4e21ce3051597bc965a9755cb1bf0
azzouz marouen <azzouz.marouen@gmail.com>
2017-09-05 01:10:47 +0200

    Update forwardPropagate.R

--- forwardPropagate.R
+++ forwardPropagate.R
@@ -1,52 +1,47 @@
 #performs forward propagation algorithm
-#### Parameters
-#weight=list containg network weights
-#X=input data
-#tt="Regression" or "Classification"
-#outF=output function
-#activation=activation function
 #### Return
-#Z=updated neurons outputs by layer
+#output=updated neurons outputs by layer
+#Z=scalar product within each neuron
 
-forwardPropagate<-function(weight,X,tt="Regression",outF="Identity",activation="sigmoid"){
+forwardPropagate<-function(weight,#neurons list
+                           X,#input data
+                           outF="Identity",#final layer activation function
+                           activation="sigmoid"#hidden layers activation
+                          ){
   L=length(weight)+1
   output=list()
+  Z=list()
   output[[1]]=as.matrix(cbind(1,X))
+  Z[[1]]=output[[1]]
   for(i in 2:L){
+    Z[[i]]=output[[i-1]]%*%as.matrix(weight[[i-1]])
     if(i<L){
-      Zi=output[[i-1]]%*%as.matrix(weight[[i-1]])
       if (activation=="sigmoid"){
-        # output[[i]]=apply(mat,2,function(x) 1/(1+exp(-output[[i-1]]%*%x)))
-        output[[i]]=1/(1+exp(-Zi))
-        
+        output[[i]]=1/(1+exp(-Z[[i]]))
       }
       if (activation=="tanh"){
-        #output[[i]]=apply(mat,2,function(x) 1.7159*tanh((2/3)*output[[i-1]]%*%x))
-        output[[i]]=1.7159*tanh((2/3)*Zi)
-        
+        output[[i]]=1.7159*tanh((2/3)*Z[[i]])
       }
       if(activation=="linear"){
-        output[[i]]=Zi
+        output[[i]]=Z[[i]]
       }
       if(activation=="ReLU"){
-        output[[i]]=Zi
+        output[[i]]=as.matrix(Z[[i]])
         output[[i]][output[[i]]<0]=0
       }
       output[[i]]=cbind(1,output[[i]])
     }
     if(i==L){
-      mat0=output[[L-1]]%*%weight[[L-1]]
-      if((outF=="Identity")&(tt=="Regression")){output[[L]]=mat0}
-      if((outF=="Sigmoid")&(tt=="Regression")){output[[L]]=1/(1+exp(-mat0))}
-      if((outF=="Tanh")&(tt=="Regression")){output[[L]]=tanh(mat0)}
-      if((outF=="Softmax")&(tt=="Regression")){output[[L]]=softmax(mat0)}
-      if((tt=="Classification") & (outF=="Softmax")){
-        K=ncol(mat0)
-        if(K==1){output[[L]]=1/(1+exp(-mat0))}
-        if(K>2){output[[L]]=softmax(mat0)}
+      if(outF=="Identity"){output[[L]]=Z[[i]]}
+      if(outF=="Sigmoid"){output[[L]]=1/(1+exp(-Z[[i]]))}
+      if(outF=="Tanh"){output[[L]]=tanh(Z[[i]])}
+      if(outF=="Softmax"){
+        K=ncol(Z[[i]])
+        if(K==1) output[[L]]=1/(1+exp(-Z[[i]]))
+        if(K>2) output[[L]]=softmax(Z[[i]])
       }
     }
   }
-  return(list(Z=output))
+  return(list(output=output,Z=Z))
 }
 
