c13934bc2849a2713f96a0f8af7acfd1788d56c0
azzouz marouen <azzouz.marouen@gmail.com>
2017-09-05 01:08:35 +0200

    Update backPropagate.R

--- backPropagate.R
+++ backPropagate.R
@@ -1,35 +1,38 @@
 #performs backpropagation algorithm
-#### Parameters
-#output=list containing neurons' output (1st element is data)
-#weight=list of weights by layer
-#resp=response vector
-#tt="Regression" or "Classification"
-#ll=loss function
-#outF=output function
-#activation=activation function
-#rr=learning rate
-#wD (weight decay)=list where 1st item is TRUE/FALSE and 2nd item is lambda value
-#trW (trace weights)=TRUE/FALSE
-#### Returns
+#### Output
 #W=updated weights list
 #D=gradient
 #tr=trace weights update (if trW)
 #gradupdate=gradient values update (if trW)
 
-backPropagate<-function(output,weight,resp,ll="RSS",outF="Identity",
-                        activation="sigmoid",rr,wD,trW,weightsVector){
+backPropagate<-function(output, #neuron output
+                        Z, #scalar product output
+                        weight,#neurons
+                        resp,#response vector
+                        ll="RSS", #loss function
+                        outF="Identity", #final layer activation
+                        activation="sigmoid",#hidden layers activation
+                        rr,#learning rate
+                        wD,#weights decay
+                        trW,weightsVector,
+                        optimization='GD',#optimization alg
+                        beta1=0.9,beta2=0.99,momentum=NULL,rmsprop=NULL, #optimization parameters
+                        tol,#numerical tolerance
+                        t #iteration count
+                       ){
   N=nrow(output[[1]])
   K=ncol(resp)
   L=length(weight)+1#total layers count
   delta=list()#delta[[1]]=NULL
+  gradRaw=list()
   tmp=list(length=L-1)#new weights
   traceW=list(length=L-1)
   gradUpdate=list()
   for(i in L:2){#weights in layer i are one index below: Z[[i]]=f(Z[[i-1]]%*%W[[i-1]])
     if(i==L){#output layer
-      mat0=output[[length(output)]]
-      deltaL=(mat0-resp)
-      if((ll=="RSS" & outF=="Identity")|(ll=="CrossEntropy" & outF=="Softmax")|(ll=="CrossEntropy" & outF=="Sigmoid")){
+      mat0=output[[L]]
+      deltaL=mat0-resp
+      if((ll=="RSS" & outF=="Identity")|(ll=="CrossEntropy" & outF=="Softmax")){
         delta[[i]]=deltaL
       }
       if(ll=="RSS" & outF=="Sigmoid"){
@@ -43,18 +46,19 @@ backPropagate<-function(output,weight,resp,ll="RSS",outF="Identity",
         mat2=delta[[i]]
         for(p in 1:K){
           for(j in 1:K){
-            mat1[,p]=mat1[,p]+mat0[,j]*mat2[,j]*(delta(p,j)-mat0[,p])
+            mat1[,p]=mat1[,p]+mat0[,j]*mat2[,j]*(delt(p,j)-mat0[,p])
           }
         }
         delta[[i]]=mat1
       }
+      #delta[[i]]=delta[[i]]%*%weightsVector
     }
     if(i<=(L-1)){#BP to hidden layers
       mat1=output[[i]][,-1]
       if(is.null(dim(mat1))){mat1=matrix(mat1,ncol=1,nrow=length(mat1))}
       if(is.null(dim(weight[[i]][2:nrow(weight[[i]]),]))){
         vec=matrix(weight[[i]][2:nrow(weight[[i]]),],
-                  ncol = nrow(weight[[i]])-1,nrow = ncol(delta[[i+1]]))
+                   ncol = nrow(weight[[i]])-1,nrow = ncol(delta[[i+1]]))
         mat2=delta[[i+1]]%*%vec
       }
       if(!is.null(dim(weight[[i]][2:nrow(weight[[i]]),]))){
@@ -62,6 +66,7 @@ backPropagate<-function(output,weight,resp,ll="RSS",outF="Identity",
       }
       if(activation=="tanh"){
         delta[[i]]=1.7159*(2/3)*(1-(mat1/1.7159)^2)*mat2
+
       }
       if(activation=="sigmoid"){
         delta[[i]]=mat1*(1-mat1)*mat2
@@ -69,14 +74,39 @@ backPropagate<-function(output,weight,resp,ll="RSS",outF="Identity",
       if(activation=="linear"){
         delta[[i]]=mat2
       }
+      if(activation=="ReLU"){
+        mat1[mat1>0]=1
+        mat1[mat1==0]=0
+        delta[[i]]=mat1*mat2
+      }
     }
-    tmp[[i-1]]=weight[[i-1]]-rr*(1/N)*t(output[[i-1]])%*%delta[[i]]
+    #gradient update
+    gradRaw[[i]]=(1/N)*t(output[[i-1]])%*%delta[[i]]
     if(wD[[1]]){#regularization
       lambda=wD[[2]]
       mat0=weight[[i-1]][2:nrow(weight[[i-1]]),]
       if(ncol(tmp[[i-1]])==1){mat0=c(0,mat0)}
       if(ncol(tmp[[i-1]])>1){mat0=rbind(0,mat0)}
-      tmp[[i-1]]=tmp[[i-1]]-rr*lambda*mat0}
+      gradRaw[[i]]=gradRaw[[i]]+lambda*mat0}
+    gradMat=matrix(0,ncol=ncol(gradRaw[[i]]))
+    
+    if(optimization=='GD') gradMat=gradRaw[[i]]
+    if(optimization%in%c('Momentum','Adam')){
+      momentum[[i-1]]=beta1*momentum[[i-1]]+(1-beta1)*gradRaw[[i]]
+      gradMat=momentum[[i-1]]
+    }
+    if(optimization%in%c('RMSProp','Adam')){
+      rmsprop[[i-1]]=beta2*rmsprop[[i-1]]+(1-beta2)*(gradRaw[[i]]^2)
+      gradMat=gradRaw[[i]]/(sqrt(rmsprop[[i-1]])+tol)
+    }
+    momentum_cor=matrix(0,ncol=ncol(gradRaw[[i]]))
+    rmsprop_cor=matrix(0,ncol=ncol(gradRaw[[i]]))
+    if(optimization=='Adam'){
+      momentum_cor=momentum[[i-1]]/(1-beta1^t)
+      rmsprop_cor=rmsprop[[i-1]]/(1-beta2^t)
+      gradMat=momentum_cor/(sqrt(rmsprop_cor)+tol)
+    }
+    tmp[[i-1]]=weight[[i-1]]-rr*gradMat
     if(trW){
       weightAbsMean=mean(abs(tmp[[i-1]][2:nrow(tmp[[i-1]]),]))
       biaisAbsMean=mean(abs(tmp[[i-1]][1,]))
@@ -86,5 +116,5 @@ backPropagate<-function(output,weight,resp,ll="RSS",outF="Identity",
       gradUpdate[[i-1]]=weight[[i-1]]-tmp[[i-1]]
     }
   }
-  return(list(W=tmp,D=delta,tr=traceW,gradupdate=gradUpdate))
+  return(list(W=tmp,D=delta,tr=traceW,gradupdate=gradUpdate,momentum=momentum,rmsprop=rmsprop))
 }
