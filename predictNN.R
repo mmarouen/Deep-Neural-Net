4a80ffb86ae0f7dbb8c9a0ecf2208e1ffc8f76cd
azzouz marouen <azzouz.marouen@gmail.com>
2017-09-05 19:10:51 +0200

    Update predictNN.R

--- predictNN.R
+++ predictNN.R
@@ -2,6 +2,6 @@
 predictNN<-function(model,X){
   L=model$argV
   out1=forwardPropagate(model$W,X,L$tt,L$outF,L$active)
-  yhatTest=transformOutput(out1$Z,L$tt,L$active,model$CL)$yhat
+  yhatTest=transformOutput(out1$output,L$tt,L$active,model$CL)$yhat
   return(yhatTest)
 }
