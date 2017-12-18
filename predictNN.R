44a0d34147f4349f51572b194f9acdbee6123e79
azzouz marouen <azzouz.marouen@gmail.com>
2017-12-18 15:20:02 +0100

    Delete predictNN.R

--- predictNN.R
+++
@@ -1,9 +0,0 @@
-#make  predictions from a generated model and preprocessed input data
-predictNN<-function(model,#model input
-                    X #input test matrix
-                   ){
-  L=model$argV
-  out1=forwardPropagate(model$W,model$b,X,L$outF,L$active,L$bnVars,model$popStats)
-  yhatTest=transformOutput(out1$Y,L$tt,model$CL)$yhat
-  return(yhatTest)
-}
